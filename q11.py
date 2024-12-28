1a0095df248d5817d5e38e246faa6c429106eff0
guilherme-marcello <guilemosm@gmail.com>
2024-12-28 22:24:23 +0000

    Included example of usage (q11 example)

---
+++ q11.py
@@ -0,0 +1,95 @@
+import argparse
+import numpy as np
+import cv2
+import os
+from scipy.io import loadmat, savemat
+import matplotlib.pyplot as plt
+
+from video import EnrichedVideo
+from vision import compute_homography
+
+class Q11Config:
+    def __init__(self, kp, map, i):
+        self.keypoint_matches = kp
+        self.google_maps_image = map
+        self.input_dir = i
+        
+
+def parse_args() -> Q11Config:
+    """
+    Parses command-line arguments for the video processing pipeline.
+
+    This function defines and parses command-line arguments 
+    necessary for the video processing pipeline.
+
+    Args:
+        None
+
+    Returns:
+        Q11Config: An object containing the parsed arguments as a Q1.1 config.
+            - keypoint_matches (-kp): Path to the keypoint matches file. 
+                                     Defaults to "kp_gmaps.mat".
+            - google_maps_image (-map): Path to the Google Maps image. 
+                                       Defaults to "gmaps_alamedaIST.png".
+            - input_dir (-v): Directory containing video frames and YOLO detections.. 
+                                    Defaults to the current directory. 
+    """
+    parser = argparse.ArgumentParser(description="PIV 1.1: Process video frames and YOLO detections.")
+    parser.add_argument("--keypoint_matches", "-kp", type=str, default="kp_gmaps.mat",
+                        help="Path to keypoint matches file.")
+    parser.add_argument("--google_maps_image", "-map",  type=str, default="gmaps_alamedaIST.png",
+                        help="Path to Google Maps image.")
+    parser.add_argument("--input_dir", "-i", type=str, default=".",
+                        help="Directory containing video frames and YOLO detections.")
+    return parser.parse_args()
+
+def main(config: Q11Config):
+    # load detections and frames as a "video" object
+    print(f"Loading files in {config.input_dir}, this may take a while...")
+    video = EnrichedVideo.from_input_directory(config.input_dir)
+
+    print(f"Loaded {len(video)} frames ({video.get_number_of_frames_with_detections()} with detections)!")
+
+    # load keypoint matches
+    matches_raw = loadmat(config.keypoint_matches) 
+
+    # this matches are regarding first image img_0001.jpg -> maps image
+    matches = matches_raw["kp_gmaps"]
+
+    # load source points
+    src_points = matches[:, :2]
+    
+    # load dst points
+    dst_points = matches[:, 2:]
+
+    """
+    first_img = video[0]
+    gmaps_img = cv2.imread(config.google_maps_image) 
+
+    plt.imshow(first_img.frame)
+    plt.plot(src_points[:, 0], src_points[:, 1], 'Xr')
+    plt.show()
+   
+    plt.imshow(gmaps_img)
+    plt.plot(dst_points[:, 0], dst_points[:, 1], 'Xr')
+    plt.show()    
+    """
+
+    # compute homography!
+    H = compute_homography(src_points, dst_points)
+    savemat("homography.mat", {"H": H})
+
+    for frame in video:
+        warped_frame = frame.transform(H)
+        warped_frame.show()
+
+    #warped_video = EnrichedVideo(frames=[frame.transform(H) for frame in video])
+
+    #for warped_frame in warped_video:
+    #    warped_frame.export("output_dir")
+    
+
+
+if __name__ == "__main__":
+    config = parse_args()
+    main(config)
